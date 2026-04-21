9f61d56f74fd46e1ab3269c7ecc44a0c8c355c54
Ridha Azizi <azizi_ridha@yahoo.fr>
2026-04-21 23:57:51 +0100

    Update variational quantum circuit to 12-layer depth to match paper specification

--- quantum/quantum_circuit.py
+++ quantum/quantum_circuit.py
@@ -1,9 +1,38 @@
 import pennylane as qml
+import numpy as np
+
+# Number of qubits after PCA reduction
 n_qubits = 8
-dev = qml.device('default.qubit', wires=n_qubits)
+
+# Circuit depth as reported in the paper
+n_layers = 12
+
+# Quantum device (simulation)
+dev = qml.device("default.qubit", wires=n_qubits)
+
 @qml.qnode(dev)
-def circuit(x, w):
-    for i in range(n_qubits): qml.RX(x[i], wires=i)
-    for i in range(n_qubits-1): qml.CNOT(wires=[i,i+1])
-    for i in range(n_qubits): qml.RY(w[i], wires=i)
+def circuit(x, weights):
+    """
+    Variational Quantum Circuit (VQC)
+    - RX encoding layer
+    - 12 variational layers (CNOT + RY)
+    - Pauli-Z measurements
+    """
+
+    # -------- Embedding layer (RX encoding) --------
+    for i in range(n_qubits):
+        qml.RX(x[i], wires=i)
+
+    # -------- Variational layers --------
+    for layer in range(n_layers):
+        # Entanglement (nearest-neighbor)
+        for i in range(n_qubits - 1):
+            qml.CNOT(wires=[i, i + 1])
+
+        # Trainable rotations
+        for i in range(n_qubits):
+            qml.RY(weights[layer * n_qubits + i], wires=i)
+
+    # -------- Measurement layer --------
     return [qml.expval(qml.PauliZ(i)) for i in range(n_qubits)]
+``
